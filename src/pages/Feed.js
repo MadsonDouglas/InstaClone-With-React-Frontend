01bba25fd3c65d602031fa3ba55244277af7bacd
MadsonDouglas <santos.madson94@gmail.com>
2019-06-12 12:32:15 -0300

    criando frontend

---
+++ src/pages/Feed.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Feed extends Component {
+    render() {
+        return (
+            <h1>
+                Feed
+            </h1>
+        )
+    }
+}
